3884d2e2a5ff3fe2e45122fc53b109cb0318fcee
Pedro Veiga Ribeiro <pedroveiga.ribeiro@gmail.com>
2024-06-09 11:27:58 -0300

    Health System

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public Action OnDeath;
+	
+	[SerializeField] float maxHealth;
+	float currentHealth;
+	
+	private void Awake()
+	{
+		currentHealth = maxHealth;
+	}
+	
+	public void IncreaseHealth(float value)
+	{
+		currentHealth += value;
+		if(currentHealth <= 0) OnDeath?.Invoke();
+		if(currentHealth > maxHealth) currentHealth = maxHealth;
+	}
+	
+	public void DecreaseHealth(float value)
+	{
+		IncreaseHealth(-value);
+	}
+	
+	public void SetNewMaxHealth(float maxHealth)
+	{
+		this.maxHealth = maxHealth;
+	}
 }
